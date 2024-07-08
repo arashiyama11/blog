daa584eaa0b6f7ef7c44b6b6a5617d9596045951
arashiyama11 <arashiyama1120@gmail.com>
2024-07-09 00:31:58 +0900

    githubから持ってくる関数つくった

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,94 +5,92 @@ import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsCompose)
-    alias(libs.plugins.compose.compiler)
+  alias(libs.plugins.kotlinMultiplatform)
+  alias(libs.plugins.androidApplication)
+  alias(libs.plugins.jetbrainsCompose)
+  alias(libs.plugins.compose.compiler)
+  kotlin("plugin.serialization") version "2.0.0"
 }
 
 kotlin {
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        moduleName = "composeApp"
-        browser {
-            commonWebpackConfig {
-                outputFileName = "composeApp.js"
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(project.projectDir.path)
-                    }
-                }
-            }
+  @OptIn(ExperimentalWasmDsl::class)
+  wasmJs {
+    moduleName = "composeApp"
+    browser {
+      commonWebpackConfig {
+        outputFileName = "composeApp.js"
+        devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+          static = (static ?: mutableListOf()).apply {
+            // Serve sources to debug inside browser
+            add(project.projectDir.path)
+          }
         }
-        binaries.executable()
+      }
     }
+    binaries.executable()
+  }
+  
+  androidTarget {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_11)
+    }
+  }
+  
+  sourceSets {
     
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
+    androidMain.dependencies {
+      implementation(compose.preview)
+      implementation(libs.androidx.activity.compose)
     }
-    
-    sourceSets {
-        
-        androidMain.dependencies {
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
-        }
-        commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-            implementation(libs.okio)
-            //implementation(libs.ktor.client.core)
-            //implementation("io.ktor:ktor-client-core:2.3.12")
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-        }
+    commonMain.dependencies {
+      implementation(compose.runtime)
+      implementation(compose.foundation)
+      implementation(compose.material)
+      implementation(compose.ui)
+      implementation(compose.components.resources)
+      implementation(compose.components.uiToolingPreview)
+      implementation(libs.okio)
+      implementation(libs.kotlinx.serialization.json)
+      implementation("org.jetbrains.kotlinx:kotlinx-io-core:0.4.0")
+      implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     }
+  }
 }
 
 android {
-    namespace = "io.github.arashiyama11"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
-    defaultConfig {
-        applicationId = "io.github.arashiyama11"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+  namespace = "io.github.arashiyama11"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
+  
+  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+  sourceSets["main"].res.srcDirs("src/androidMain/res")
+  sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+  
+  defaultConfig {
+    applicationId = "io.github.arashiyama11"
+    minSdk = libs.versions.android.minSdk.get().toInt()
+    targetSdk = libs.versions.android.targetSdk.get().toInt()
+    versionCode = 1
+    versionName = "1.0"
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+  }
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
     }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildFeatures {
-        compose = true
-    }
-    dependencies {
-        debugImplementation(compose.uiTooling)
-    }
-}
-dependencies {
-    //implementation(libs.ktor.ktor.client.core)
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  buildFeatures {
+    compose = true
+  }
+  dependencies {
+    debugImplementation(compose.uiTooling)
+  }
 }
--- composeApp/src/commonMain/kotlin/HttpClient.kt
+++ composeApp/src/commonMain/kotlin/HttpClient.kt
@@ -1,1 +1,44 @@
-expect suspend fun fetch(url:String):String
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.*
+import okio.ByteString.Companion.decodeBase64
+
+//TODO　エラーハンドリングやる
+expect suspend fun fetch(url: String): String
+
+@Serializable
+private data class Links(val self: String, val git: String, val html: String)
+
+@Serializable
+private data class Article(
+  val name: String,
+  val path: String,
+  val sha: String,
+  val size: Int,
+  val url: String,
+  val html_url: String,
+  val git_url: String,
+  val download_url: String,
+  val type: String,
+  val _links: Links
+)
+
+@Serializable
+private data class ArticleDetail(val content: String)
+
+private val withUnknownKeys = Json { ignoreUnknownKeys = true }
+
+//記事の名前のリストを返す
+suspend fun getArticleNameList(): List<String> {
+  val response = fetch("https://api.github.com/repos/arashiyama11/blog/contents/articles")
+  val articleJson =
+    withUnknownKeys.decodeFromString<@Serializable List<Article>>(response)
+  return articleJson.map { it.name }
+}
+
+//記事のmarkdownを取得
+suspend fun getArticleMd(name: String): String {
+  val response =
+    withUnknownKeys.decodeFromString<ArticleDetail>(fetch("https://api.github.com/repos/arashiyama11/blog/contents/articles/$name"))
+  return response.content.decodeBase64()!!.utf8()
+  //TODO エラーハンドリング!!
+}
