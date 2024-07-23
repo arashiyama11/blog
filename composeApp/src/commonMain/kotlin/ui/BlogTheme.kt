23f48958c268a09e4371eec6a3a191eee3eb0764
arashiyama11 <arashiyama1120@gmail.com>
2024-07-23 23:07:04 +0900

    fontの引っ越し

--- composeApp/src/commonMain/kotlin/ui/BlogTheme.kt
+++ composeApp/src/commonMain/kotlin/ui/BlogTheme.kt
@@ -4,14 +4,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import tools.jPFontFamily
+import blog.composeapp.generated.resources.Res
+import blog.composeapp.generated.resources.japanese
+import org.jetbrains.compose.resources.Font
 
 @Composable
 fun BlogTheme(content: @Composable () -> Unit) {
   val typography = Typography(
-    defaultFontFamily = jPFontFamily(),
+    defaultFontFamily = FontFamily(Font(resource = Res.font.japanese)),
     h1 = TextStyle(
       fontWeight = FontWeight.Bold,
       fontSize = 48.sp,
