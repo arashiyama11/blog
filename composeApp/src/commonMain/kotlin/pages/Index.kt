020c37611c58b45e307b3375edc6c11b4452fef7
arashiyama11 <arashiyama1120@gmail.com>
2024-09-13 23:19:01 +0900

    update pages

--- composeApp/src/commonMain/kotlin/pages/Index.kt
+++ composeApp/src/commonMain/kotlin/pages/Index.kt
@@ -2,12 +2,13 @@ package pages
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -24,63 +25,63 @@ import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun Index() {
-  LazyColumn(
-    Modifier.fillMaxWidth(),
-    horizontalAlignment = Alignment.CenterHorizontally,
-    contentPadding = PaddingValues(6.dp)
-  ) {
-    item { Text("arashiyama", style = MaterialTheme.typography.h1) }
-    item { Spacer(modifier = Modifier.height(100.dp)) }
-    item {
-      Text(
-        "About Me",
-        style = MaterialTheme.typography.h3,
-        textDecoration = TextDecoration.Underline
-      )
-      Spacer(Modifier.height(30.dp))
-      Text(
-        """筑波大学に通っている人
+    Column(
+        Modifier.fillMaxWidth().verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text("arashiyama", style = MaterialTheme.typography.h1)
+        Spacer(modifier = Modifier.height(100.dp))
+        Text(
+            "About Me",
+            style = MaterialTheme.typography.h3,
+            textDecoration = TextDecoration.Underline
+        )
+        Spacer(Modifier.height(30.dp))
+        Text(
+            """筑波大学に通っている人
 Kotlinと米津玄師が好き
 Web開発,Androidアプリ開発あたりを少しやっています。
 暇なときはAtCoderで競技プログラミングしたり、機械学習周り触ってます。
 あとバドミントンやってます。""", style = MaterialTheme.typography.body1
-      )
+        )
+        Spacer(modifier = Modifier.height(100.dp))
+
+        Text(
+            "Tech Stack",
+            style = MaterialTheme.typography.h3,
+            textDecoration = TextDecoration.Underline
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            "触ったことがあるぐらいを意味します",
+            color = Color.Gray,
+            style = MaterialTheme.typography.body2
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+            Text("・Programming languages", style = MaterialTheme.typography.h5)
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Image(painterResource(Res.drawable.langs), null)
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+            Text("・Dev tools", style = MaterialTheme.typography.h5)
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Image(painterResource(Res.drawable.dev_tools), null)
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            "I'm interested in",
+            style = MaterialTheme.typography.h3,
+            textDecoration = TextDecoration.Underline
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Image(painterResource(Res.drawable.ints), null, modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(20.dp))
     }
-    item { Spacer(modifier = Modifier.height(100.dp)) }
-    
-    item {
-      Text(
-        "Tech Stack",
-        style = MaterialTheme.typography.h3,
-        textDecoration = TextDecoration.Underline
-      )
-      Text(
-        "触ったことがあるぐらいを意味します",
-        color = Color.Gray,
-        style = MaterialTheme.typography.body2
-      )
-      Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
-        Text("・Programming languages", style = MaterialTheme.typography.h5)
-      }
-      Image(painterResource(Res.drawable.langs), null)
-      Spacer(modifier = Modifier.height(20.dp))
-      Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
-        Text("・Dev tools", style = MaterialTheme.typography.h5)
-      }
-      Image(painterResource(Res.drawable.dev_tools), null)
-      Spacer(modifier = Modifier.height(20.dp))
-      
-      Text(
-        "I'm interested in",
-        style = MaterialTheme.typography.h3,
-        textDecoration = TextDecoration.Underline
-      )
-      
-      Spacer(modifier = Modifier.height(20.dp))
-      
-      Image(painterResource(Res.drawable.ints), null, modifier = Modifier.fillMaxWidth())
-      Spacer(modifier = Modifier.height(20.dp))
-    }
-    item { Spacer(modifier = Modifier.height(100.dp)) }
-  }
 }
--- composeApp/src/commonMain/kotlin/pages/Profile.kt
+++ composeApp/src/commonMain/kotlin/pages/Profile.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,103 +27,95 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 @Composable
 fun Profile() {
-  LazyColumn(
-    modifier = Modifier.fillMaxSize(),
-    horizontalAlignment = Alignment.CenterHorizontally
-  ) {
-    
-    item {
-      Text(
-        "Profile", style = MaterialTheme.typography.h1,
-        textDecoration = TextDecoration.Underline
-      )
-      Spacer(Modifier.height(50.dp))
-      profileElem("Name", "Yuto Nakamura")
-      profileElem("Handle name", "arashiyama or kamu")
-      val birthday = LocalDate.parse("2006-02-11")
-      val age = birthday.yearsUntil(Clock.System.todayIn(TimeZone.UTC))
-      profileElem("Age", age.toString())
-      profileElem("Birthday", "Feb 11, 2006")
-      profileElem("School", "University of Tsukuba,\nCollege of Information Science")
-      profileElem("Live in", "Tukuba City, Ibaraki, Japan")
-      profileElem("From", "Yokohama City, Kanagawa, Japan")
-      Spacer(Modifier.height(50.dp))
+    Column(
+        modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Profile", style = MaterialTheme.typography.h1,
+            textDecoration = TextDecoration.Underline
+        )
+        Spacer(Modifier.height(50.dp))
+        profileElem("Name", "Yuto Nakamura")
+        profileElem("Handle name", "arashiyama or kamu")
+        val birthday = LocalDate.parse("2006-02-11")
+        val age = birthday.yearsUntil(Clock.System.todayIn(TimeZone.UTC))
+        profileElem("Age", age.toString())
+        profileElem("Birthday", "Feb 11, 2006")
+        profileElem("School", "University of Tsukuba,\nCollege of Information Science")
+        profileElem("Live in", "Tukuba City, Ibaraki, Japan")
+        profileElem("From", "Yokohama City, Kanagawa, Japan")
+        Spacer(Modifier.height(50.dp))
+        Text(
+            "Tech Stack",
+            style = MaterialTheme.typography.h3,
+            textDecoration = TextDecoration.Underline
+        )
+
+        techStackElem(
+            "JavaScript",
+            "一通りかけるけどUI操作は苦手。Reactあたりのフレームワークまともに使ったことないのでいつか使いたいと思ってる。手元の計算機とか簡単なツールは大体Node.jsで書いてる"
+        )
+        techStackElem("HTML,CSS", "わかんないことはわかる")
+        techStackElem(
+            "GoogleAppsScript",
+            "割とわかる。定期実行したいツールの作成はコレ。マジ便利。一時期ココナラで小銭稼ぎしてた"
+        )
+        techStackElem(
+            "Kotlin",
+            "一番好き。素直でクセがなくて書いてて気持ちいい。全てをKotlinで書きたいと思っている"
+        )
+        techStackElem("Android dev", "わかんないけど、理解したい。Androidアプリを作る構想はある")
+        techStackElem(
+            "Jetpack Compose",
+            "Multiplatform含めて結構触ってる。ModifierはCSSより素直な感じがして良い。Compose Multiplatformでこのページ書いてる。書き心地良いしDesktopツール作るならコレ使おうと思ってる"
+        )
+        techStackElem(
+            "Python",
+            "競プロと機械学習専用言語。あんま好きじゃない。再代入禁止ぐらいさせてほしい"
+        )
+
+        techStackElem(
+            "Haskell",
+            "マジでわからん。高校2年生当時、関数型言語とやらに興味を持つが、2,3ヶ月で挫折。とはいえ副作用や参照透過性,遅延評価の考え方と向き合えていい経験になったと思っている"
+        )
+
+        techStackElem("Git", "すこしわかる")
+        techStackElem(
+            "GitHub",
+            "よくわかんないままActionsとか書いてる。多分このサービスの10%ぐらいは使えてる"
+        )
     }
-    
-    item {
-      Text(
-        "Tech Stack",
-        style = MaterialTheme.typography.h3,
-        textDecoration = TextDecoration.Underline
-      )
-      
-      techStackElem(
-        "JavaScript",
-        "一通りかけるけどUI操作は苦手。Reactあたりのフレームワークまともに使ったことないのでいつか使いたいと思ってる。手元の計算機とか簡単なツールは大体Node.jsで書いてる"
-      )
-      techStackElem("HTML,CSS", "わかんないことはわかる")
-      techStackElem(
-        "GoogleAppsScript",
-        "割とわかる。定期実行したいツールの作成はコレ。マジ便利。一時期ココナラで小銭稼ぎしてた"
-      )
-      techStackElem(
-        "Kotlin",
-        "一番好き。素直でクセがなくて書いてて気持ちいい。全てをKotlinで書きたいと思っている"
-      )
-      techStackElem("Android dev", "わかんないけど、理解したい。Androidアプリを作る構想はある")
-      techStackElem(
-        "Jetpack Compose",
-        "Multiplatform含めて結構触ってる。ModifierはCSSより素直な感じがして良い。Compose Multiplatformでこのページ書いてる。書き心地良いしDesktopツール作るならコレ使おうと思ってる"
-      )
-      techStackElem(
-        "Python",
-        "競プロと機械学習専用言語。あんま好きじゃない。再代入禁止ぐらいさせてほしい"
-      )
-      
-      techStackElem(
-        "Haskell",
-        "マジでわからん。高校2年生当時、関数型言語とやらに興味を持つが、2,3ヶ月で挫折。とはいえ副作用や参照透過性,遅延評価の考え方と向き合えていい経験になったと思っている"
-      )
-      
-      techStackElem("Git", "すこしわかる")
-      techStackElem(
-        "GitHub",
-        "よくわかんないままActionsとか書いてる。多分このサービスの10%ぐらいは使えてる"
-      )
-    }
-    
-    item {
-      Spacer(modifier = Modifier.height(50.dp))
-    }
-  }
+    Spacer(modifier = Modifier.height(50.dp))
 }
 
 @Composable
 fun profileElem(a: String, b: String) = Row(modifier = Modifier.fillMaxWidth()) {
-  Text(a.padEnd(20), modifier = Modifier.width(160.dp), style = MaterialTheme.typography.body1)
-  Text(b, style = MaterialTheme.typography.body2)
-  Spacer(Modifier.height(30.dp))
+    Text(a.padEnd(20), modifier = Modifier.width(160.dp), style = MaterialTheme.typography.body1)
+    Text(b, style = MaterialTheme.typography.body2)
+    Spacer(Modifier.height(30.dp))
 }
 
 
 @Composable
 fun techStackElem(name: String, content: String) =
-  Column(modifier = Modifier.fillMaxWidth()) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-      Text("- ", style = MaterialTheme.typography.body1)
-      Text(
-        name,
-        style = MaterialTheme.typography.body1,
-        textDecoration = TextDecoration.Underline
-      )
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text("- ", style = MaterialTheme.typography.body1)
+            Text(
+                name,
+                style = MaterialTheme.typography.body1,
+                textDecoration = TextDecoration.Underline
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                content,
+                style = MaterialTheme.typography.body2
+            )
+        }
+        Spacer(modifier = Modifier.height(40.dp))
     }
-    Row(modifier = Modifier.fillMaxWidth()) {
-      Spacer(modifier = Modifier.width(10.dp))
-      Text(
-        content,
-        style = MaterialTheme.typography.body2
-      )
-    }
-    Spacer(modifier = Modifier.height(40.dp))
-  }
 
