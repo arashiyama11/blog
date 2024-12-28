0d969bd2acc745b8f4022cd5f09c2c9235a2177c
arashiyama11 <arashiyama1120@gmail.com>
2024-12-28 23:05:52 +0900

    refactor footer

--- composeApp/src/commonMain/kotlin/ui/Footer.kt
+++ composeApp/src/commonMain/kotlin/ui/Footer.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,20 +11,19 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
+import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -34,128 +32,104 @@ import blog.composeapp.generated.resources.GitHubMark
 import blog.composeapp.generated.resources.XMark
 import blog.composeapp.generated.resources.Res
 import blog.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import tools.JumpExternalPage
 import tools.kmpLocalContext
 
 @Composable
-fun Footer(modifier: Modifier) = Column(modifier = Modifier.fillMaxWidth()) {
-  val jumpExternalPage=JumpExternalPage(kmpLocalContext())
-  Row(
-    modifier = Modifier.fillMaxWidth().weight(3f)
-      .border(
-        width = 1.dp,
-        color = MaterialTheme.colors.onBackground,
-        shape = RoundedCornerShape(20.dp)
-      )
-  ) {
-    Spacer(modifier = Modifier.fillMaxHeight().weight(1f))
-    Column(
-      modifier = Modifier.fillMaxHeight().weight(24f),
-      verticalArrangement = Arrangement.spacedBy(6.dp)
+fun Footer(modifier: Modifier) = Column(modifier = modifier.fillMaxWidth()) {
+    val jumpExternalPage = JumpExternalPage(kmpLocalContext())
+    Row(
+        modifier = Modifier.fillMaxWidth().weight(3f)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colors.onBackground,
+                shape = RoundedCornerShape(20.dp)
+            )
     ) {
-      Text(" Links", style = MaterialTheme.typography.h3)
-      linkCard("X Account", Res.drawable.XMark) {
-        jumpExternalPage("https://x.com/kamu_coins",)
-      }
-      linkCard(
-        "GitHub Profile",
-        Res.drawable.GitHubMark
-      ) { jumpExternalPage("https://github.com/arashiyama11") }
-      linkCard(
-        "This Site's Repository",
-        Res.drawable.GitHubMark
-      ) { jumpExternalPage("https://github.com/arashiyama11/blog") }
+        Spacer(modifier = Modifier.fillMaxHeight().weight(1f))
+        Column(
+            modifier = Modifier.fillMaxHeight().weight(24f),
+            verticalArrangement = Arrangement.spacedBy(6.dp)
+        ) {
+            Text(" Links", style = MaterialTheme.typography.h3)
+            ExPageLink("X Account", { Icon(painterResource(Res.drawable.XMark), null) }) {
+                jumpExternalPage("https://x.com/kamu_coins")
+            }
+            ExPageLink(
+                "GitHub Profile",
+                { Icon(painterResource(Res.drawable.GitHubMark), null) }
+            ) { jumpExternalPage("https://github.com/arashiyama11") }
+            ExPageLink(
+                "This Site's Repository",
+                { Icon(painterResource(Res.drawable.GitHubMark), null) }
+            ) { jumpExternalPage("https://github.com/arashiyama11/blog") }
+        }
+        Spacer(modifier = Modifier.fillMaxHeight().weight(2f))
+        Column(
+            modifier = Modifier.fillMaxHeight().weight(24f),
+            verticalArrangement = Arrangement.spacedBy(6.dp)
+        ) {
+            Text(
+                " Contact",
+                style = MaterialTheme.typography.h3
+            )
+
+            ExPageLink("X Direct Message",
+                { Icon(painterResource(Res.drawable.XMark), null) }
+            ) {
+                jumpExternalPage("https://x.com/kamu_coins")
+            }
+            ExPageLink("arashiyama1120 @gmail.com",
+                { Icon(Icons.Default.Email, null) }
+            ) {}
+        }
+        Spacer(modifier = Modifier.fillMaxHeight().weight(1f))
+
     }
-    Spacer(modifier = Modifier.fillMaxHeight().weight(2f))
+    //コピーライト
     Column(
-      modifier = Modifier.fillMaxHeight().weight(24f),
-      verticalArrangement = Arrangement.spacedBy(6.dp)
+        modifier = Modifier.fillMaxSize().weight(1f)
+            .border(1.dp, MaterialTheme.colors.onBackground, shape = RoundedCornerShape(20.dp))
     ) {
-      Text(
-        " Contact",
-        style = MaterialTheme.typography.h3
-      )
-      linkCard("X Direct Message", Res.drawable.XMark) {
-        jumpExternalPage("https://x.com/kamu_coins")
-      }
-      linkCard("arashiyama1120 @gmail.com", Icons.Default.Email) {}
+        Spacer(Modifier.weight(1f))
+        Text(
+            "Copyright © 2024 arashiyama All Rights Reserved",
+            modifier = Modifier.weight(8f).fillMaxWidth().wrapContentSize(),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h6
+        )
+        Row(modifier = Modifier.weight(8f).fillMaxWidth().wrapContentWidth()) {
+            Text(
+                "Powered by Compose Multiplatform",
+                modifier = Modifier,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h6
+            )
+            Image(painterResource(Res.drawable.compose_multiplatform), "compose_multiplatform")
+        }
+        Spacer(Modifier.weight(1f))
     }
-    Spacer(modifier = Modifier.fillMaxHeight().weight(1f))
-    
-  }
-  //コピーライト
-  Column(
-    modifier = Modifier.fillMaxSize().weight(1f)
-      .border(1.dp, MaterialTheme.colors.onBackground, shape = RoundedCornerShape(20.dp))
-  ) {
-    Spacer(Modifier.weight(1f))
-    Text(
-      "Copyright © 2024 arashiyama All Rights Reserved",
-      modifier = Modifier.weight(8f).fillMaxWidth().wrapContentSize(),
-      textAlign = TextAlign.Center,
-      style = MaterialTheme.typography.h6
-    )
-    Row(modifier = Modifier.weight(8f).fillMaxWidth().wrapContentWidth()) {
-      Text(
-        "Powered by Compose Multiplatform",
-        modifier = Modifier,
-        textAlign = TextAlign.Center,
-        style = MaterialTheme.typography.h6
-      )
-      Image(painterResource(Res.drawable.compose_multiplatform), "compose_multiplatform")
-    }
-    Spacer(Modifier.weight(1f))
-  }
 }
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-private fun linkCard(text: String, imageRes: DrawableResource, onClick: () -> Unit) = Card(
-  modifier = Modifier.fillMaxWidth().height(42.dp)
-    .border(
-      width = 1.dp,
-      color = MaterialTheme.colors.onBackground,
-      shape = RoundedCornerShape(6.dp)
-    )
-    .clickable { onClick() }, shape = RoundedCornerShape(6.dp), elevation = 3.dp
-) {
-  Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.Start) {
-    Image(
-      painterResource(imageRes),
-      text,
-      modifier = Modifier.height(42.dp).width(42.dp)
-    )
-    Box(modifier = Modifier.fillMaxHeight().wrapContentHeight()) {
-      Text(
-        " $text",
-        fontSize = 16.sp,
-        fontWeight = FontWeight.ExtraBold
-      )
+private fun ExPageLink(text: String, icon: @Composable () -> Unit, onClick: () -> Unit) =
+    ListItem(
+        modifier = Modifier.fillMaxWidth().height(42.dp)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colors.onBackground,
+                shape = RoundedCornerShape(6.dp)
+            )
+            .clickable { onClick() }.clip(RoundedCornerShape(6.dp)),
+        icon = icon
+    ) {
+        Text(
+            " $text",
+            fontSize = 16.sp,
+            lineHeight = 18.sp,
+            fontWeight = FontWeight.ExtraBold,
+        )
     }
-  }
-}
-
-@Composable
-private fun linkCard(text: String, imageRes: ImageVector, onClick:  () -> Unit) = Card(
-  modifier = Modifier.fillMaxWidth().height(42.dp)
-    .border(
-      width = 1.dp,
-      color = MaterialTheme.colors.onBackground,
-      shape = RoundedCornerShape(6.dp)
-    )
-    .clickable { onClick() }, shape = RoundedCornerShape(6.dp), elevation = 3.dp
-) {
-  Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.Start) {
-    Icon(imageRes, text, modifier = Modifier.height(42.dp).width(42.dp))
-    Box(modifier = Modifier.fillMaxHeight().wrapContentHeight()) {
-      Text(
-        " $text",
-        fontSize = 16.sp,
-        lineHeight = 18.sp,
-        fontWeight = FontWeight.ExtraBold,
-      )
-    }
-  }
-}
